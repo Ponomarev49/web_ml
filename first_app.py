9ed342e2e5c74f073af08573324d7e982c9627f0
Ponomarev49 <ponomarevm143@gmail,com>
2024-01-11 07:11:44 +0600

    remake visualisation

--- first_app.py
+++ first_app.py
@@ -9,6 +9,8 @@ from sklearn.metrics import accuracy_score
 from pandas.plotting import scatter_matrix
 from keras.models import load_model
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
 model_path = 'Models/'
 
 # Загрузка датасета
@@ -92,36 +94,51 @@ def page_dataset():
 def page_data_visualization():
     st.title("Визуализации данных")
 
-    st.subheader("Тепловая карта корреляции")
-    fig_heatmap, ax_heatmap = plt.subplots(figsize=(14, 11))
-    correlation_matrix = data.corr()
-    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=.5, ax=ax_heatmap)
-    plt.savefig("heatmap.png")
-    st.image("heatmap.png")
+    # plt.figure(figsize=(12, 8))
+    # selected_cols = ['bomb_planted', 'time_left', 'ct_health', 't_health', 'ct_players_alive', 't_players_alive']
+    # selected_df = data[selected_cols]
+    # sns.heatmap(selected_df.corr(), annot=True, cmap='coolwarm')
+    # plt.title('Тепловая карта с корреляцией')
+    # plt.savefig(f"Images/heatmap.png")
+    st.image(f"Images/heatmap.png")
 
     st.subheader("Гистограммы")
-    for feature in data:
-        fig_hist = plt.figure()
-        sns.histplot(data=data, x=feature, kde=True)
-        plt.title(f"Гистограмма для {feature}")
-        plt.savefig(f"Images/Hist/hist_{feature}.png")
-        st.image(f"Images/Hist/hist_{feature}.png")
+
+    def plot_histogram(column_name):
+        # plt.figure(figsize=(8, 6))
+        # plt.hist(data[column_name], bins=20, color='skyblue', edgecolor='black')
+        # plt.title(f'Гистограмма {column_name}')
+        # plt.xlabel(column_name)
+        # plt.ylabel('Частота')
+        # plt.savefig(f"Images/Hist/hist_{column_name}.png")
+        st.image(f"Images/Hist/hist_{column_name}.png")
+
+    # Отображение выбора колонки и гистограммы
+    selected_column_hist = st.selectbox('Выберите колонку', data.columns, key="select_hist")
+    plot_histogram(selected_column_hist)
 
     st.subheader("Боксплоты")
-    columns = list(data.columns)
-    plt.figure(figsize=(20, 20))
-    for column in columns:
-        plt.subplot(4, 4, columns.index(column)+1)
-        sns.boxplot(data=data, y=column)
-        plt.title(column)
-    plt.savefig("Images/boxplot.png")
-    st.image("Images/boxplot.png")
 
-    st.subheader("Матрица диаграмм рассеяния")
-    matrix_features = ['bomb_planted', 'ct_players_alive', 't_players_alive']
-    scatter_matrix_fig = scatter_matrix(data[matrix_features], figsize=(12, 12), alpha=0.8, diagonal='hist')
-    plt.savefig("Images/scatter_matrix.png")
-    st.image("Images/scatter_matrix.png")
+    def plot_boxplot(column_name):
+        # plt.figure(figsize=(8, 6))
+        # plt.boxplot(data[column_name])
+        # plt.title(f'Ящик с усами для {column_name}')
+        # plt.xlabel(column_name)
+        # plt.ylabel('Значения')
+        # plt.savefig(f"Images/Boxplot/box_{column_name}.png")
+        st.image(f"Images/Boxplot/box_{column_name}.png")
+
+    # Отображение выбора колонки и гистограммы
+    selected_column_box = st.selectbox('Выберите колонку', data.columns, key="select_box")
+    plot_boxplot(selected_column_box)
+
+    st.subheader("Круговая диаграмма целевого признака")
+    # plt.figure(figsize=(8, 8))
+    # data['bomb_planted'].value_counts().plot.pie(autopct='%1.1f%%')
+    # plt.title('Bomb planted')
+    # plt.ylabel('')
+    # plt.savefig(f"Images/Pie.png")
+    st.image(f"Images/Pie.png")
 
 
 # Страница с инференсом моделей
